2659e45ad6413f31c504ddb444aebb80996bbbe1
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-10-29 17:24:14 +0200

    Show players in database as DataTable widget in new_page page

--- lib/pages/new_game.dart
+++ lib/pages/new_game.dart
@@ -2,21 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:unocounter/widgets/app_bar.dart';
 import 'package:unocounter/widgets/buttons.dart';
 
-class NewGamePage extends StatelessWidget {
+class NewGamePage extends StatefulWidget {
   const NewGamePage({super.key});
 
   @override
+  State<NewGamePage> createState() => _NewGamePageState();
+}
+
+class _NewGamePageState extends State<NewGamePage> {
+  List<Map<String, dynamic>> data = [
+    {'name': 'John Doe', 'winnableGames': 10},
+    {'name': 'Jane Doe', 'winnableGames': 20},
+    {'name': 'Bob Smith', 'winnableGames': 30},
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSelected();
+  }
+
+  void _initializeSelected() {
+    setState(() {
+      data = data.map((row) {
+        row['selected'] = false;
+        return row;
+      }).toList();
+    });
+  }
+
+  void _onCheckboxChanged(int index, bool? value) {
+    setState(() {
+      data[index]['selected'] = value ?? false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: Show here created people(players) as table with switch(include in game or not), and a form for new player
     return Scaffold(
       appBar: CustomAppBar(title: 'New Game Page'),
-      body: Center(
-        child: CustomButton(
-          text: 'Go back!',
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+      body: Column(
+        children: [
+          DataTable(
+            columns: [
+              DataColumn(label: Text('Name')),
+              DataColumn(label: Text('Number of Winnable Games')),
+              DataColumn(label: Text('Select')),
+            ],
+            rows: data.asMap().entries.map((entry) {
+              int index = entry.key;
+              Map<String, dynamic> row = entry.value;
+              return DataRow(
+                cells: [
+                  DataCell(Text(row['name'])),
+                  DataCell(Text(row['winnableGames'].toString())),
+                  DataCell(Switch(
+                    value: row['selected'],
+                    onChanged: (bool value) {
+                      _onCheckboxChanged(index, value);
+                    },
+                  )),
+                ],
+              );
+            }).toList(),
+          ),
+          Center(
+            child: CustomButton(
+              text: 'Go back!',
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
