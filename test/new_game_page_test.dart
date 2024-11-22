ee8a11f60634b0011471703c2fcf4bbbde38fcad
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-11-22 09:49:42 +0200

    Divide tests

--- test/new_game_page_test.dart
+++ test/new_game_page_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 import 'package:unocounter/models/player.dart';
-import 'package:unocounter/pages/games.dart';
 import 'package:unocounter/pages/new_game.dart';
 import 'package:unocounter/providers/player_provider.dart';
 import 'package:unocounter/repositories/player_repository.dart';
@@ -13,49 +12,6 @@ import 'package:unocounter/widgets/buttons.dart';
 class MockPlayerRepository extends Mock implements PlayerRepository {}
 
 void main() {
-  group('GamesPage', () {
-    testWidgets('should render GamesPage with data', (tester) async {
-      final games = [
-        Game(name: 'Game 1', players: ['Player 1', 'Player 2']),
-        Game(name: 'Game 2', players: ['Player 3', 'Player 4']),
-      ];
-
-      await tester.pumpWidget(MaterialApp(home: GamesPage(games: games)));
-
-      expect(find.byType(GamesPage), findsOneWidget);
-      expect(find.widgetWithText(CustomAppBar, 'Games Page'), findsOneWidget);
-
-      // Verify game data
-      for (final game in games) {
-        expect(find.text(game.name), findsOneWidget);
-        expect(find.text(game.players.join(', ')), findsOneWidget);
-      }
-    });
-
-    testWidgets('should render loading indicator', (tester) async {
-      await tester.pumpWidget(MaterialApp(home: GamesPage(isLoading: true)));
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
-    });
-
-    testWidgets('should render error message', (tester) async {
-      await tester
-          .pumpWidget(MaterialApp(home: GamesPage(error: 'Test Error')));
-      expect(find.text('Test Error'), findsOneWidget);
-    });
-
-    testWidgets('should render no games message', (tester) async {
-      await tester.pumpWidget(MaterialApp(home: GamesPage(games: [])));
-      expect(find.text('No games found'), findsOneWidget);
-    });
-
-    testWidgets('Go back button pops the route', (tester) async {
-      await tester.pumpWidget(MaterialApp(home: GamesPage()));
-      await tester.tap(find.widgetWithText(CustomButton, 'Go back!'));
-      await tester.pumpAndSettle();
-      expect(find.byType(GamesPage), findsNothing);
-    });
-  });
-
   group('NewGamePage', () {
     late MockPlayerRepository mockPlayerRepository;
     final playerMaps = <String, PlayerSerializer>{
