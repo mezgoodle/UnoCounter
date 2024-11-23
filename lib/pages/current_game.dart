5f537632284dfb5aee3220339d93a4a506a6806f
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-11-23 12:06:28 +0200

    Create game page

---
+++ lib/pages/current_game.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:unocounter/models/player.dart';
+import 'package:unocounter/widgets/app_bar.dart';
+
+class CurrentGamePage extends StatelessWidget {
+  const CurrentGamePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final currentDealer = PlayerSerializer.fromMap({
+      "name": "John Doe",
+      "winnableGames": 10,
+    });
+    const movesMade = 10;
+    return Scaffold(
+      appBar: CustomAppBar(title: 'Current Game'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Moves made: $movesMade',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Current Dealer: ${currentDealer.name}',
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
