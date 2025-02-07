3372a15eabe9fd72552a5a4d426fff83f3f90b84
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-02-07 22:57:05 +0200

    Add gameprovider to render data

--- lib/pages/games.dart
+++ lib/pages/games.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unocounter/models/game.dart';
+import 'package:unocounter/providers/game_provider.dart';
 import 'package:unocounter/widgets/app_bar.dart';
 import 'package:unocounter/widgets/buttons.dart';
 
@@ -10,67 +13,92 @@ class Game {
 }
 
 class GamesPage extends StatelessWidget {
-  final List<Game> games;
-  final bool isLoading;
-  final String? error;
-
-  const GamesPage({
-    Key? key,
-    this.games = const [
-      Game(name: 'Game 1', players: ['Player 1', 'Player 2']),
-      Game(name: 'Game 2', players: ['Player 1', 'Player 2']),
-      Game(name: 'Game 3', players: ['Player 1', 'Player 2']),
-      Game(name: 'Game 4', players: ['Player 1', 'Player 2']),
-      Game(name: 'Game 5', players: ['Player 1', 'Player 2']),
-    ],
-    this.isLoading = false,
-    this.error,
-  }) : super(key: key);
+  const GamesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return const Center(child: CircularProgressIndicator());
-    }
-    if (error != null) {
-      return Center(child: Text(error!));
-    }
-    if (games.isEmpty) {
-      return const Center(child: Text('No games found'));
-    }
+    // if (isLoading) {
+    //   return const Center(child: CircularProgressIndicator());
+    // }
+    // if (error != null) {
+    //   return Center(child: Text(error!));
+    // }
+    // if (games.isEmpty) {
+    //   return const Center(child: Text('No games found'));
+    // }
     return Scaffold(
       appBar: CustomAppBar(title: 'Games Page'),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
-                columns: [
-                  DataColumn(label: Text('Game')),
-                  DataColumn(label: Text('Players')),
-                ],
-                rows: games.map((row) {
-                  return DataRow(
-                    cells: [
-                      DataCell(Text(row.name)),
-                      DataCell(Text(row.players.join(', '))),
-                    ],
-                  );
-                }).toList(),
-              ),
+      body: Consumer<GameProvider>(builder: (context, gameProvider, _) {
+        if (gameProvider.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (gameProvider.hasError) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.error_outline, size: 48, color: Colors.red),
+                const SizedBox(height: 16),
+                Text(
+                  'Failed to load players',
+                  style: Theme.of(context).textTheme.titleMedium,
+                )
+              ],
             ),
-            Center(
-              child: CustomButton(
-                text: 'Go back!',
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+          );
+        }
+        if (gameProvider.games.isEmpty) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'No games found',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 16),
+                // CustomButton(
+                //   text: 'Add Player',
+                //   onPressed: () => _showAddPlayerDialog(context),
+                // ),
+              ],
             ),
-          ],
-        ),
-      ),
+          );
+        }
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: [
+                    DataColumn(label: Text('Game')),
+                    DataColumn(label: Text('Players')),
+                  ],
+                  rows: gameProvider.games.asMap().entries.map((entry) {
+                    // int index = entry.key;
+                    GameSerializer row = entry.value;
+                    return DataRow(
+                      cells: [
+                        DataCell(Text(row.id.toString())),
+                        DataCell(Text(row.players!.join(', '))),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              ),
+              Center(
+                child: CustomButton(
+                  text: 'Go back!',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
