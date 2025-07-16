b0b37960c0122e2f5d8ae7e5354bc2a2bba77e7a
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-07-16 20:18:58 +0300

    Cu 8696t9mm5 create a collection (#8)
    * Create game model and repository
    
    * Create simple provider
    
    * Add gameprovider to render data
    
    * Add games provider
    
    * Show players
    
    * Fix typing
    
    * Update pages
    
    * Delete all files

--- lib/constants/values.dart
+++
@@ -1,9 +0,0 @@
-/// Constants related to game configuration and rules
-abstract class GameConstants {
-  /// Maximum number of players allowed in a game.
-  /// Limited to 6 players to ensure:
-  /// - Reasonable game duration
-  /// - Balanced gameplay
-  /// - Optimal user interface layout
-  static const int maximumSelectedPlayers = 6;
-}
--- lib/repositories/repository.dart
+++
@@ -1,6 +0,0 @@
-abstract class IRepository<T> {
-  add(T item);
-  Future<void> update(String id, T item);
-  delete(String id);
-  getAll();
-}
