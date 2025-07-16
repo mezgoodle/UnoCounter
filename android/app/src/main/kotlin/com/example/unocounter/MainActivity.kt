8f3efae7c6985500328a812ec9dd92213be85b1c
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-07-16 20:12:08 +0300

    Delete all files

--- android/app/src/main/kotlin/com/example/unocounter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.unocounter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/constants/assets.dart
+++
@@ -1,3 +0,0 @@
-class Assets {
-  static const String logoPath = 'images/logo.png';
-}
