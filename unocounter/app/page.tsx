f613db49e45aaeb177972b3205a006a6cfa0f8fb
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-01-08 10:49:27 +0200

    Ignore some problem

--- unocounter/app/page.tsx
+++ unocounter/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [showCreateForm, setShowCreateForm] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setGames(getGames());
   }, []);
 
